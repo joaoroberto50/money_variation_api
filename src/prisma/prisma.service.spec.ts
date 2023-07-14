4c6ff972697cde2c5cc0b23c3a2d6c7c22fdf1e6
Joao Robert <joao@email.com>
2023-07-14 18:20:19 -0300

    tests

--- src/prisma/prisma.service.spec.ts
+++ src/prisma/prisma.service.spec.ts
@@ -1,18 +1,44 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { PrismaService } from './prisma.service';
+import { INestApplication } from '@nestjs/common';
 
 describe('PrismaService', () => {
-  let service: PrismaService;
+  let prismaService: PrismaService;
+  let mockApp: Partial<INestApplication>;
+  let mockOn: jest.Mock;
 
   beforeEach(async () => {
+    mockApp = {
+      close: jest.fn(),
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService],
     }).compile();
 
-    service = module.get<PrismaService>(PrismaService);
+    prismaService = module.get<PrismaService>(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('should connect to the database on module initialization', async () => {
+    const connectSpy = jest.spyOn(prismaService, '$connect');
+
+    await prismaService.onModuleInit();
+
+    expect(connectSpy).toHaveBeenCalled();
+  });
+
+  it('should enable shutdown hooks with correct application close', () => {
+    const onSpy = jest.spyOn(prismaService, '$on');
+
+    prismaService.enableShutdownHooks(mockApp as INestApplication);
+
+    expect(onSpy).toHaveBeenCalledWith('beforeExit', expect.any(Function));
+
+    const eventHandler = onSpy.mock.calls[0][1];
+
+    const mockBeforeExitHandler = jest.fn();
+    eventHandler(mockBeforeExitHandler);
+
+    expect(mockApp.close).toHaveBeenCalled();
   });
 });
