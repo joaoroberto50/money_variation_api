85c4d5482e8b5fb697ace9e990d81e41cfdb3645
Joao Robert <joao@email.com>
2023-07-14 15:08:56 -0300

    Http verbs and Swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, HttpStatus } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('RootEntryPoint')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @ApiResponse({ status: HttpStatus.OK })
+  @ApiOperation({ summary: "Returns initial application information." })
+  getHello() {
+    return {
+      statusStatus: HttpStatus.OK,
+      data: this.appService.getHello(),
+    };
   }
 }
--- src/money/money.controller.ts
+++ src/money/money.controller.ts
@@ -1,17 +1,30 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, HttpStatus, Param } from '@nestjs/common';
 import { MoneyService } from './money.service';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('money')
 @Controller('money')
 export class MoneyController {
   constructor(private readonly MoneyService: MoneyService) {}
 
   @Get()
+  @ApiResponse({ status: HttpStatus.OK })
+  @ApiOperation({ summary: "Returns all currencies with updated values." })
   findAll() {
-    return this.MoneyService.values;
+    return {
+      statusCode: HttpStatus.OK,
+      data: this.MoneyService.values,
+    }
   }
 
+  @ApiTags('money/{name}')
   @Get(':name')
+  @ApiResponse({ status: HttpStatus.OK })
+  @ApiOperation({ summary: "Returns the updated value of a specific currency." })
   findOne(@Param('name') name: string) {
-    return this.MoneyService.findOne(name);
+    return {
+      statusCode: HttpStatus.OK,
+      data: this.MoneyService.findOne(name),
+    };
   }
 }
